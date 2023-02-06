670613ce404badf3b39cc09f5b235e44c45f4a91
ijlalbm <muhammadijlalm@gmail.com>
2023-02-06 15:30:31 +0800

    connect to mongodb atlas cloud database

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
 
 require('dotenv/config')
 
 const api = process.env.API_URL
 
 // Middleware
-// Used for control req and respon data fromtge server, the old is bodyparser
+// Used for control req and respon data from server, the old is bodyparser
 app.use(express.json())
+// used for showing log access in api endpoint
+app.use(morgan('tiny'))
 
 app.get(`${api}/products`,(req,res)=>{
     const product = {
@@ -19,11 +23,17 @@ app.get(`${api}/products`,(req,res)=>{
 })
 
 app.post(`${api}/products`,(req,res)=>{
-    //get data from user post
+    //get data from user edp post
     const newProduct = req.body;
     console.log(newProduct);
     res.send(newProduct);
 })
+mongoose.set('strictQuery',false);
+mongoose.connect(process.env.CONNECTION_STRING).then(()=>{
+    console.log('Database Ready')
+}).catch((err)=>{
+    console.log(err);
+});
 
 app.listen(3000,()=>{
     console.log('Server is running http://localhost:3000');
